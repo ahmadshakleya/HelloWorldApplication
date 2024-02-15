e913f0ed70ede5e6f280904917fb05c9ec60f26b
ThomasKramp <thomas.kramp378@gmail.com>
2024-02-15 12:38:24 +0100

    Add client & server

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,17 @@
+import java.io.IOException;
+import java.net.Socket;
+import java.util.concurrent.TimeUnit;
+
+// netstat -ant | grep 127
+// https://www.geeksforgeeks.org/socket-programming-in-java/
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, Ahmad!");
+    public static void main(String[] args) throws IOException {
+        System.out.println("System start");
+        try {
+            Socket socket = new Socket("127.0.0.1", 2680);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
---
+++ src/main/java/TCP_Client.java
@@ -0,0 +1,59 @@
+// A Java program for a Client
+import java.io.*;
+import java.net.*;
+
+public class TCP_Client {
+    // initialize socket and input output streams
+    private Socket socket = null;
+    private DataInputStream input = null;
+    private DataOutputStream out = null;
+
+    // constructor to put ip address and port
+    public TCP_Client(String address, int port) throws UnknownHostException {
+        // establish a connection
+        try {
+            socket = new Socket(address, port);
+            System.out.println("Connected");
+
+            // takes input from terminal
+            input = new DataInputStream(System.in);
+
+            // sends output to the socket
+            out = new DataOutputStream(
+                    socket.getOutputStream());
+        } catch (IOException i) {
+            System.out.println(i);
+            return;
+        }
+
+        // string to read message from input
+        String line = "";
+
+        // keep reading until "Over" is input
+        while (!line.equals("Over")) {
+            try {
+                line = input.readLine();
+                out.writeUTF(line);
+            } catch (IOException i) {
+                System.out.println(i);
+            }
+        }
+
+        // close the connection
+        try {
+            input.close();
+            out.close();
+            socket.close();
+        } catch (IOException i) {
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String args[]) throws UnknownHostException {
+        try {
+            TCP_Client client = new TCP_Client("127.0.0.1", 5000);
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
